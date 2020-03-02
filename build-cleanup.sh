d7f8f8daee3d55c809e20226d4c97872834bdd94
danielpickett <daniel@danielpickett.me>
2020-03-02 12:36:21 -0500

    yup

---
+++ build-cleanup.sh
@@ -0,0 +1,3 @@
+echo "export * from './components'" > dist/index.d.ts
+echo "export * from './components'" > dist/index.js
+rm dist/react-app-env.d.js
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './tokens/tokens.css'
+import { NumberInput } from './'
+
+ReactDOM.render(<NumberInput />, document.getElementById('root'));
+
+export * from './components'
