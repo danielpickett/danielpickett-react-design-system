30ab7203599ffc4b2d9f7c1f5325e7c154178de4
danielpickett <daniel@danielpickett.me>
2020-03-04 15:38:14 -0500

    this

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,31 @@ import React, { useState } from 'react'
 import { NumberInput } from 'components'
 
 export const App = () => {
-  const [number1, setNumber1] = useState(5)
-  const [number2, setNumber2] = useState(10)
+  const [minNum, setMinNum] = useState(5)
+  const [maxNum, setMaxNum] = useState(10)
 
-  const handleOnChange1 = (val: number) => {
-    setNumber1(val)
+  const handleUpdateMinNum = (val: number) => {
+    setMinNum(val)
     console.log(val)
   }
 
-  const handleOnChange2 = (val: number) => {
-    setNumber2(val)
+  const handleUpdateMaxNum = (val: number) => {
+    setMaxNum(val)
     console.log(val)
   }
 
   return (
     <div>
       <span>Donate between</span>{' '}
-      <NumberInput onChange={handleOnChange1} value={number1} />{' '}
+      <NumberInput
+        onChange={handleUpdateMinNum}
+        value={minNum}
+        // max={7}
+        // min={2}
+      />{' '}
       <span>and</span>{' '}
-      <NumberInput onChange={handleOnChange2} value={number2} />{' '}
-      <span>ostrich prides</span>{' '}
+      <NumberInput onChange={handleUpdateMaxNum} value={maxNum} />{' '}
+      <span>ostriches</span>{' '}
     </div>
   )
 }
--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -1,13 +1,34 @@
-import React, { SyntheticEvent } from 'react'
+import React, { FormEvent, useState } from 'react'
 import './NumberInput.css'
 
 export const NumberInput: React.FunctionComponent<{
   onChange: (arg: number) => void
   value: number
-}> = ({ onChange, value }) => {
-  const handleOnChange = (event: SyntheticEvent) => {
-    
+  step?: number
+  // max?: number
+  // min?: number
+}> = ({ onChange, value, step = 1 }) => {
+  const [isEmpty, setIsEmpty] = useState(false)
+
+  const handleOnChange = (event: FormEvent<HTMLInputElement>) => {
+    const newValue = event.currentTarget.value
+    const regex = /[^.0-9]+/g
+
+    if (newValue.match(regex)) {
+      return
+    } else if (newValue === '') {
+      setIsEmpty(true)
+    } else {
+      setIsEmpty(false)
+      onChange(parseFloat(newValue))
+    }
   }
+
+  const handleIncrement = (increment: number) => {
+    console.log('increment', increment)
+    onChange(value + increment)
+  }
+
   return (
     <div className="NumberInput">
       <div className="NumberInput__container">
@@ -15,11 +36,19 @@ export const NumberInput: React.FunctionComponent<{
           type="text"
           onChange={event => handleOnChange(event)}
           className="NumberInput__input"
-          value={value}
+          value={isEmpty ? '' : value}
+          placeholder={isEmpty ? value.toString() : ''}
+          onBlur={() => setIsEmpty(false)}
         />
         <div className="NumberInput__arrows">
-          <button className="NumberInput__up-arrow"></button>
-          <button className="NumberInput__down-arrow"></button>
+          <button
+            className="NumberInput__up-arrow"
+            onClick={() => handleIncrement(step)}
+          ></button>
+          <button
+            className="NumberInput__down-arrow"
+            onClick={() => handleIncrement(-step)}
+          ></button>
         </div>
       </div>
     </div>
