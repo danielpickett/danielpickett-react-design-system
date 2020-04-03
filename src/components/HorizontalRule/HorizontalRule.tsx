48573c52887e40340691521b9ca0563ad3463df5
danielpickett <daniel@danielpickett.me>
2020-04-03 12:58:45 -0400

    phew! working again

---
+++ src/components/HorizontalRule/HorizontalRule.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './HorizontalRule.scss'
+
+export const HorizontalRule: React.FunctionComponent<{}> = ({}) => {
+  return (
+    <div className="HorizontalRule" >
+    </div>
+  )
+}
