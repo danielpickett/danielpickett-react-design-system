506afbbb60eba3c90e9572c001fec8261fc95db1
John Le <johnminhle01@gmail.com>
2020-04-21 16:47:20 -0400

    Add Modal (#4)
    Add Modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,43 @@
+import React, { MouseEvent, useEffect, CSSProperties, FC } from 'react'
+import classNames from 'classnames'
+import './Modal.scss'
+
+type ModalProps = {
+  offClick?: () => void
+  noBackground?: boolean
+  className?: string
+  style?: CSSProperties
+}
+
+export const Modal: FC<ModalProps> = ({
+  offClick,
+  children,
+  noBackground = false,
+  className,
+  style = {}
+}) => {
+  const handleClick = (e: MouseEvent) => {
+    e.stopPropagation()
+  }
+
+  useEffect(() => {
+    window.document.body.style.overflow = 'hidden'
+    return () => {
+      window.document.body.style.overflow = 'auto'
+    }
+  }, [])
+
+  return (
+    <div className="Modal" onClick={offClick}>
+      <div
+        className={classNames('content-Modal', className, {
+          noBackground: noBackground,
+        })}
+        onClick={handleClick}
+        style={style}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
