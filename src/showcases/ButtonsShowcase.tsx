4f9ebaaf1d5699bbfd0bb446725c0605f23dfd3f
danielpickett <daniel@danielpickett.me>
2020-05-13 11:11:47 -0400

    😊

--- src/showcases/ButtonsShowcase.tsx
+++ src/showcases/ButtonsShowcase.tsx
@@ -1,69 +1,141 @@
-import React from 'react'
-import { Button, Text } from '../components'
+import React, { useState } from 'react'
+import { Button, Text, Checkbox } from '../components'
 import { Calendar, Camera, Edit } from 'react-feather'
 
 export const ButtonsShowcase = () => {
+  const [disabled, setDisabled] = useState(false)
+
   return (
-    <div
-      className="ButtonsShowcase"
-      style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(10, max-content)',
-        gap: '1rem',
-        justifyItems: 'start'
-      }}
-    >
-      {/* <Text>Small</Text>
-      <Button size="extra-small" >Default</Button>
-      <Button size="extra-small" kind="primary">Primary</Button>
-      <Button size="extra-small" kind="danger">Danger</Button>
-      <Button size="extra-small" IconLeft={Camera}>Icon Left</Button>
-      <Button size="extra-small" IconLeft={Calendar}>Icon Right</Button>
-      <Button size="extra-small" rounded>Rounded</Button> */}
+    <div>
+      <Checkbox
+        checked={disabled}
+        onChange={(checked) => {
+          setDisabled(checked)
+        }}
+        label="Disable all buttons"
+      />
+      <div
+        className="ButtonsShowcase"
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(10, max-content)',
+          gap: '1rem',
+          justifyItems: 'start',
+        }}
+      >
+        <Text>Small</Text>
+        <Button disabled={disabled} size="small">Default</Button>
+        <Button disabled={disabled} size="small" kind="primary">
+          Primary
+        </Button>
+        <Button disabled={disabled} size="small" kind="danger">
+          Danger
+        </Button>
+        <Button disabled={disabled} size="small" IconLeft={Camera}>
+          Icon Left
+        </Button>
+        <Button disabled={disabled} size="small" IconRight={Calendar}>
+          Icon Right
+        </Button>
+        <Button disabled={disabled} size="small" rounded>
+          Rounded
+        </Button>
+        <Button disabled={disabled} size="small" kind="text">
+          text
+        </Button>
+        <Button disabled={disabled} size="small" kind="text" rounded IconLeft={Edit}>
+          text
+        </Button>
+        <Button disabled={disabled} size="small" kind="text" rounded IconLeft={Edit} />
 
-      <Text>Small</Text>
-      <Button size="small" >Default</Button>
-      <Button size="small" kind="primary">Primary</Button>
-      <Button size="small" kind="danger">Danger</Button>
-      <Button size="small" IconLeft={Camera}>Icon Left</Button>
-      <Button size="small" IconRight={Calendar}>Icon Right</Button>
-      <Button size="small" rounded>Rounded</Button>
-      <Button size="small" kind="text" >text</Button>
-      <Button size="small" kind="text" rounded IconLeft={Edit}>text</Button>
-      <Button size="small" kind="text" rounded IconLeft={Edit}/>
+        <Text>Medium</Text>
+        <Button disabled={disabled}>Default</Button>
+        <Button disabled={disabled} kind="primary">Primary</Button>
+        <Button disabled={disabled} kind="danger">Danger</Button>
+        <Button disabled={disabled} IconLeft={Camera}>Icon Left</Button>
+        <Button disabled={disabled} IconRight={Calendar}>Icon Right</Button>
+        <Button disabled={disabled} rounded>Rounded</Button>
+        <Button disabled={disabled} kind="text">text</Button>
+        <Button disabled={disabled} kind="text" rounded IconLeft={Edit}>
+          text
+        </Button>
+        <Button disabled={disabled} kind="text" rounded IconLeft={Edit} />
 
-      <Text>Medium</Text>
-      <Button>Default</Button>
-      <Button kind="primary">Primary</Button>
-      <Button kind="danger">Danger</Button>
-      <Button IconLeft={Camera}>Icon Left</Button>
-      <Button IconRight={Calendar}>Icon Right</Button>
-      <Button rounded>Rounded</Button>
-      <Button kind="text" >text</Button>
-      <Button kind="text" rounded IconLeft={Edit}>text</Button>
-      <Button kind="text" rounded IconLeft={Edit}/>
-      
-      <Text>Large</Text>
-      <Button size="large" >Default</Button>
-      <Button size="large" kind="primary">Primary</Button>
-      <Button size="large" kind="danger">Danger</Button>
-      <Button size="large" IconLeft={Camera}>Icon Left</Button>
-      <Button size="large" IconRight={Calendar}>Icon Right</Button>
-      <Button size="large" rounded>Rounded</Button>
-      <Button size="large" kind="text" >text</Button>
-      <Button size="large" kind="text" rounded IconLeft={Edit}>text</Button>
-      <Button size="large" kind="text" rounded IconLeft={Edit}/>
-      
-      <Text>Extra Large</Text>
-      <Button size="extra-large">Default</Button>
-      <Button size="extra-large" kind="primary">Primary</Button>
-      <Button size="extra-large" kind="danger">Danger</Button>
-      <Button size="extra-large" IconLeft={Camera}>Icon Left</Button>
-      <Button size="extra-large" IconRight={Calendar}>Icon Right</Button>
-      <Button size="extra-large" rounded>Rounded</Button>
-      <Button size="extra-large" kind="text" >text</Button>
-      <Button size="extra-large" kind="text" rounded IconLeft={Edit}>text</Button>
-      <Button size="extra-large" kind="text" rounded IconLeft={Edit}/>
+        <Text>Large</Text>
+        <Button disabled={disabled} size="large">Default</Button>
+        <Button disabled={disabled} size="large" kind="primary">
+          Primary
+        </Button>
+        <Button disabled={disabled} size="large" kind="danger">
+          Danger
+        </Button>
+        <Button disabled={disabled} size="large" IconLeft={Camera}>
+          Icon Left
+        </Button>
+        <Button disabled={disabled} size="large" IconRight={Calendar}>
+          Icon Right
+        </Button>
+        <Button disabled={disabled} size="large" rounded>
+          Rounded
+        </Button>
+        <Button disabled={disabled} size="large" kind="text">
+          text
+        </Button>
+        <Button disabled={disabled} size="large" kind="text" rounded IconLeft={Edit}>
+          text
+        </Button>
+        <Button disabled={disabled} size="large" kind="text" rounded IconLeft={Edit} />
+
+        <Text>Extra Large</Text>
+        <Button disabled={disabled} size="extra-large">Default</Button>
+        <Button disabled={disabled} size="extra-large" kind="primary">
+          Primary
+        </Button>
+        <Button disabled={disabled} size="extra-large" kind="danger">
+          Danger
+        </Button>
+        <Button disabled={disabled} size="extra-large" IconLeft={Camera}>
+          Icon Left
+        </Button>
+        <Button disabled={disabled} size="extra-large" IconRight={Calendar}>
+          Icon Right
+        </Button>
+        <Button disabled={disabled} size="extra-large" rounded>
+          Rounded
+        </Button>
+        <Button disabled={disabled} size="extra-large" kind="text">
+          text
+        </Button>
+        <Button disabled={disabled} size="extra-large" kind="text" rounded IconLeft={Edit}>
+          text
+        </Button>
+        <Button disabled={disabled} size="extra-large" kind="text" rounded IconLeft={Edit} />
+
+        <Text>Extra Large</Text>
+        <Button disabled={disabled} size="extra-large">Default</Button>
+        <Button disabled={disabled} size="extra-large" kind="primary">
+          Primary
+        </Button>
+        <Button disabled={disabled} size="extra-large" kind="danger">
+          Danger
+        </Button>
+        <Button disabled={disabled} size="extra-large" IconLeft={Camera}>
+          Icon Left
+        </Button>
+        <Button disabled={disabled} size="extra-large" IconRight={Calendar}>
+          Icon Right
+        </Button>
+        <Button disabled={disabled} size="extra-large" rounded>
+          Rounded
+        </Button>
+        <Button disabled={disabled} size="extra-large" kind="text">
+          text
+        </Button>
+        <Button disabled={disabled} size="extra-large" kind="text" rounded IconLeft={Edit}>
+          text
+        </Button>
+        <Button disabled={disabled} size="extra-large" kind="text" rounded IconLeft={Edit} />
+      </div>
     </div>
   )
 }
