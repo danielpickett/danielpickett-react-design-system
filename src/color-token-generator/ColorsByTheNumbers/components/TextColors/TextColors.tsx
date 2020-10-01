dc8affb1f0850936981a4b3971a66be4a1e11780
Daniel Pickett <daniel@danielpickett.me>
2020-10-01 09:06:43 -0400

    Rework colors (#13)
    * Adding routes (#11)
    
    * added routes for showcases
    
    * some extra nav styling, plus an "all components" route
    
    * adding np package
    
    * reverting color token change
    
    * wip: TextShowcase
    
    * methodically working through manual text color assignments
    
    * first round finished for text on color swatches
    
    * refactor + scales on grey started
    
    * a bit more work for colors on grey
    
    * border adjustment
    
    * finished text on colors work
    
    * restructure design token files

---
+++ src/color-token-generator/ColorsByTheNumbers/components/TextColors/TextColors.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './TextColors.scss'
+import { TextSample } from '../TextSample'
+
+export const TextColors = ({
+  swNode,
+  scale,
+  on,
+  wash,
+}: {
+  swNode: React.RefObject<HTMLDivElement>
+  scale: string
+  on: string
+  wash?: string
+}) => (
+  <div
+    className="TextColors"
+    style={{
+      borderLeftColor: `var(--text-color-${scale}-regular-on-${on}${
+        wash ? `-${wash}` : ''
+      })`,
+    }}
+  >
+    <TextSample
+      swatchRef={swNode}
+      color={`text-color-${scale}-regular-on-${on}${wash ? `-${wash}` : ''}`}
+    />
+    <TextSample
+      swatchRef={swNode}
+      color={`text-color-${scale}-subdued-on-${on}${wash ? `-${wash}` : ''}`}
+    />
+    <TextSample
+      swatchRef={swNode}
+      color={`text-color-${scale}-dangerously-subdued-on-${on}${
+        wash ? `-${wash}` : ''
+      }`}
+    />
+  </div>
+)
---
+++ src/color-token-generator/readme.md
@@ -0,0 +1,3 @@
+# Generating Color Tokens
+
+I use the components in this folder to generate the color tokens for the design system. I've generated the palette and all the text colors through a set of SASS files. But ultimately, the final tokens are just cut and pasted from the DevTools CSS inspector.
